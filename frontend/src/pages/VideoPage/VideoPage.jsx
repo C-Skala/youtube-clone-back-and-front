95788fda6b8d1c05cac7b5f14bbfdcfcfb3415f9
C-Skala <cjskala@gmail.com>
2022-09-20 23:02:23 -0400

    added new page to be the video player page

---
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import useCustomForm from "../../hooks/useAuth";
+import axios from "axios";
+
+
+
+const showVideos = (props) => {
+    //const [user, token] = useAuth()
+    //const navigate = useNavigate()
+    //const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues)
+
+
+
+    return ( 
+        <h1>Hello New Page!</h1>
+     );
+}
+ 
+export default showVideos;
